98b768279f3fc5f5d076e30277b1ff15e63b6a52
3145396301 <3145396301@qq.com>
2024-04-17 21:13:19 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer:{
+    proxy:{
+      'gen':{
+        target:'http://localhost:8080',
+        changeOrigin:true
+      },
+      'check':{
+        target:'http://localhost:8080',
+        changeOrigin:true
+      },
+    }
+  }
+
 })
